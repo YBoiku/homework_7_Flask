553838ebc41c2e47ba486ca1698d3338a9f74612
yehor <yehorboiku@gmail.com>
2022-06-02 17:38:28 +0300

    0.2

--- test_2.py
+++ test_2.py
@@ -1,59 +1,25 @@
-import json
-from faker import Faker
-from random import choice
-
-fake = Faker()
-
-
-# def fake_email(number: int):
-#     with open('some.json', 'w') as file:
-#         names = ['Name', 'E-mail']
-#         list_fake_name = []
-#         list_fake_email = []
-#         domain_name = ['gmail', 'yahoo']
-#         numbers = range(100)
-#         for _ in range(number):
-#             fake_name = []
-#             fake_first_and_last_name = fake.name()
-#             fake_name += [fake_first_and_last_name.split(' ')[0]]
-#             list_fake_name.append(fake_name)
-#             for each_name in fake_name:
-#                 name = each_name.lower()
-#                 create_fake_email = []
-#                 create_fake_email += [f'{name}_{choice(numbers)}@{choice(domain_name)}.com\n']
-#                 list_fake_email.append(create_fake_email)
-#         file.write(json.dumps({"Name": list_fake_name}))
-#         file.write(json.dumps({"E-mail": list_fake_email}))
-#         # writer.writeheader()
-#         # for each_fake_name in list_fake_name:
-#         #     writer.writerow({"Name": each_fake_name})
-#         # for each_fake_email in list_fake_email:
-#         #     writer.writerow({"E-mail": each_fake_email})
-#     # return writer
-def fake_email(number: int):
-    with open('some.json', 'w') as file:
+@app.route('/generate-users/<int:number>')
+def fake_email(number):
+    with open('some.csv', 'w') as file:
         names = ['Name', 'E-mail']
+        writer = csv.DictWriter(file, fieldnames=names)
         list_fake_name = []
         list_fake_email = []
         domain_name = ['gmail', 'yahoo']
         numbers = range(100)
         for _ in range(number):
-            fake_name = []
-            fake_first_and_last_name = fake.name()
-            fake_name += [fake_first_and_last_name.split(' ')[0]]
-            list_fake_name.append(fake_name)
-            for each_name in fake_name:
-                name = each_name.lower()
-                create_fake_email = []
-                create_fake_email += [f'{name}_{choice(numbers)}@{choice(domain_name)}.com\n']
-                list_fake_email.append(create_fake_email)
-        file.write(json.dumps({"Name": list_fake_name}))
-        file.write(json.dumps({"E-mail": list_fake_email}))
-        # writer.writeheader()
-        # for each_fake_name in list_fake_name:
-        #     writer.writerow({"Name": each_fake_name})
-        # for each_fake_email in list_fake_email:
-        #     writer.writerow({"E-mail": each_fake_email})
-    # return writer
-
-print(fake_email(10))
+            fake_first_and_last_name = fake.first_name()
+            list_fake_name += [f'<p>{fake_first_and_last_name}']
+            name = fake_first_and_last_name.lower()
+            list_fake_email += [f'{name}_{choice(numbers)}@{choice(domain_name)}.com<p>']
+        writer.writeheader()
+        for each_fake_name in list_fake_name:
+            for each_fake_email in list_fake_email:
+                writer.writerow({"Name": each_fake_name, "E-mail": each_fake_email})
+    with open('some.csv', 'r') as file:
+        read = csv.DictReader(file)
+        file_1 = []
+        for row in read:
+            file_1 += row['Name'], row['E-mail']
+        file.close()
+    return " ".join(file_1)
